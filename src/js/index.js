ecfd381306ba63d7b582849272f5b498c39a22ec
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-03-25 23:05:41 -0400

    DRY-ed up a lot of code with .data(). Added game results display and reset button. Made a couple functions self-invoking

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,20 @@
 // Javascript Entry Point
 
+import $ from 'jquery';
+
+//////improvements roadmap
+
+/////place data attribute either using .data(element, key, value) or manually in html
+
+/////get data attribute's value of clicked item
+
+/////put all possible players into array
+
+/////filter array for the player whose name value is equal to the value of clicked on item
+
+/////generalize the addPlayer function, make it a method in the constructor, and call for player that comes through the filter
+
+var allPossiblePlayers = [];
 
 class NicolasCage {
 	constructor(attackMultiplier, dodgeMultiplier){
@@ -14,12 +29,16 @@ class NicolasCage {
 		this.dodgeAbility = function(){
 			return Math.random() * this.dodgeMultiplier;
 		};
+		this.toArray = function(){allPossiblePlayers.push(this);};
+		this.toArray();
 	}
 }
 
+
+
 var realNicolasCage = new NicolasCage(10, 2);
-realNicolasCage.name = "real Nicolas Cage";
-// realNicolasCage.attackAbility(10);
+realNicolasCage.name = "The Real Nicolas Cage";
+
 
 var nationalTreasureNicolasCage = new NicolasCage(10, 5);
 nationalTreasureNicolasCage.name = "National Treasure Nicolas Cage";
@@ -32,15 +51,16 @@ wickerManNicolasCage.name = "Wicker Man Nicolas Cage";
 
 var ghostRiderNicolasCage = new NicolasCage(18, 8);
 ghostRiderNicolasCage.name = "Ghost Rider Nicolas Cage";
+// console.log(allPossiblePlayers);
 
+//////can be refactored to use data-madeupname = "data-here" and jquery's data() method 
+//with an event listener on a container instead of individual img's
 
-
-
-var selectNicolasCage = document.querySelector('#nicolas-cage');
-var selectNationalTreasure = document.querySelector('#national-treasure');
-var selectLordOfWar = document.querySelector('#lord-of-war');
-var selectGhost = document.querySelector('#ghost-rider');
-var selectWicker = document.querySelector('#wicker-man');
+// var selectNicolasCage = document.querySelector('#nicolas-cage');
+// var selectNationalTreasure = document.querySelector('#national-treasure');
+// var selectLordOfWar = document.querySelector('#lord-of-war');
+// var selectGhost = document.querySelector('#ghost-rider');
+// var selectWicker = document.querySelector('#wicker-man');
 
 
 var playerInfo = document.querySelector('.player-info div');
@@ -48,78 +68,95 @@ var opponentInfo = document.querySelector('.opponent-info div');
 
 var thePlayers = [];
 
+$( ".nic-cage-picker" ).on( "click", "img", function( event ) {    
 
-selectNicolasCage.onclick = function addPlayer (){
-	if(thePlayers.length === 0){
-		playerInfo.innerHTML = "The Real Nicolas Cage";
-		selectNicolasCage.className = "userSelected";
+    var selectedPlayers = allPossiblePlayers.filter( obj => {
+					    	return obj.name === $(event.target).data("name");
+					    });
+
+    thePlayers.push(selectedPlayers);
+
+    if(thePlayers.length === 1){
+		playerInfo.innerHTML = `${$(event.target).data("name")}`;
+		$(event.target).addClass("userSelected");
 	}else{
-		opponentInfo.innerHTML = "The Real Nicolas Cage";
-		selectNicolasCage.className = "computerPlayerSelected";
+		opponentInfo.innerHTML = `${$(event.target).data("name")}`;
+		$(event.target).addClass("computerPlayerSelected");
 	}
+});
 
-	thePlayers.push(realNicolasCage);
+    
 
-	
-};
+// selectNicolasCage.onclick = function addPlayer (){
+// 	if(thePlayers.length === 0){
+// 		playerInfo.innerHTML = "The Real Nicolas Cage";
+// 		selectNicolasCage.className = "userSelected";
+// 	}else{
+// 		opponentInfo.innerHTML = "The Real Nicolas Cage";
+// 		selectNicolasCage.className = "computerPlayerSelected";
+// 	}
 
-selectNationalTreasure.onclick = function addPlayer (){
-	if(thePlayers.length === 0){
-		playerInfo.innerHTML = "National Treasure Nicolas Cage";
-		selectNationalTreasure.className = "userSelected";
-	}else{
-		opponentInfo.innerHTML = "National Treasure Nicolas Cage";
-		selectNationalTreasure.className = "computerPlayerSelected";
-	}
+// 	thePlayers.push(realNicolasCage);
 
-	thePlayers.push(nationalTreasureNicolasCage);
-};
+// };
 
-selectLordOfWar.onclick = function addPlayer (){
-	if(thePlayers.length === 0){
-		playerInfo.innerHTML = "Lord of War Nicolas Cage";
-		selectLordOfWar.className = "userSelected";
-	}else{
-		opponentInfo.innerHTML = "Lord of War Nicolas Cage";
-		selectLordOfWar.className = "computerPlayerSelected";
-	}
+// selectNationalTreasure.onclick = function addPlayer (){
+// 	if(thePlayers.length === 0){
+// 		playerInfo.innerHTML = "National Treasure Nicolas Cage";
+// 		selectNationalTreasure.className = "userSelected";
+// 	}else{
+// 		opponentInfo.innerHTML = "National Treasure Nicolas Cage";
+// 		selectNationalTreasure.className = "computerPlayerSelected";
+// 	}
 
-	thePlayers.push(lordOfWarNicolasCage);
-};
+// 	thePlayers.push(nationalTreasureNicolasCage);
+// };
 
-selectGhost.onclick = function addPlayer (){
-	if(thePlayers.length === 0){
-		playerInfo.innerHTML = "Ghost Rider Nicolas Cage";
-		selectGhost.className = "userSelected";
-	}else{
-		opponentInfo.innerHTML = "Ghost Rider Nicolas Cage";
-		selectGhost.className = "computerPlayerSelected";
-	}
+// selectLordOfWar.onclick = function addPlayer (){
+// 	if(thePlayers.length === 0){
+// 		playerInfo.innerHTML = "Lord of War Nicolas Cage";
+// 		selectLordOfWar.className = "userSelected";
+// 	}else{
+// 		opponentInfo.innerHTML = "Lord of War Nicolas Cage";
+// 		selectLordOfWar.className = "computerPlayerSelected";
+// 	}
 
-	thePlayers.push(ghostRiderNicolasCage);
-};
+// 	thePlayers.push(lordOfWarNicolasCage);
+// };
 
-selectWicker.onclick = function addPlayer (){
-	if(thePlayers.length === 0){
-		playerInfo.innerHTML = "Wicker Man Nicolas Cage";
-		selectWicker.className = "userSelected";
-	}else{
-		opponentInfo.innerHTML = "Wicker Man Nicolas Cage";
-		selectWicker.className = "computerPlayerSelected";
-	}
+// selectGhost.onclick = function addPlayer (){
+// 	if(thePlayers.length === 0){
+// 		playerInfo.innerHTML = "Ghost Rider Nicolas Cage";
+// 		selectGhost.className = "userSelected";
+// 	}else{
+// 		opponentInfo.innerHTML = "Ghost Rider Nicolas Cage";
+// 		selectGhost.className = "computerPlayerSelected";
+// 	}
 
-	thePlayers.push(wickerManNicolasCage);
-};
+// 	thePlayers.push(ghostRiderNicolasCage);
+// };
+
+// selectWicker.onclick = function addPlayer (){
+// 	if(thePlayers.length === 0){
+// 		playerInfo.innerHTML = "Wicker Man Nicolas Cage";
+// 		selectWicker.className = "userSelected";
+// 	}else{
+// 		opponentInfo.innerHTML = "Wicker Man Nicolas Cage";
+// 		selectWicker.className = "computerPlayerSelected";
+// 	}
+
+// 	thePlayers.push(wickerManNicolasCage);
+// };
 
 
 
 
-var userScore = document.querySelector('#userScore');
-var computerNicScore = document.querySelector('#computerNicScore');
+var $theScores = $(document.querySelector('#the-scores'));
 var roundNumber = document.querySelector('#round');
 
 
 var button = document.querySelector('button');
+var $gameResultsContainer = $(document.querySelector('.game-results-container'));
 var gameResults = document.querySelector('.game-results');
 var resetButton = document.querySelector('#reset');
 
@@ -131,49 +168,47 @@ var round =0;
 button.onclick = function updateScore (){
 
 	round+=1;
-	roundNumber.innerHTML = `Round ${round}:`
+	roundNumber.innerHTML = `Round ${round}:`;
 	
-	function userStatus(){
-		if (thePlayers[0].dodgeAbility() >= thePlayers[1].attackAbility()){
-				userScore.innerHTML = "Your Nicolas Cage dodged the attack";
+	(function userStatus(){
+		if (thePlayers[0][0].dodgeAbility() >= thePlayers[1][0].attackAbility()){
+				$theScores.html("<div>You dodged the attack!</div>");
 				return currentUserScore;
 				
 		}else{
-			currentUserScore -= thePlayers[1].attackAbility();
-			userScore.innerHTML = `${thePlayers[0].name}'s remaining hit points ${currentUserScore}`;
+			currentUserScore -= thePlayers[1][0].attackAbility();
+			$theScores.html(`<div>${thePlayers[0][0].name}'s remaining hit points ${currentUserScore}</div>`);
 		}
-	}
-	userStatus();
+	})();
 
 
-	function computerNicStatus(){
-		if (thePlayers[1].dodgeAbility() >= thePlayers[0].attackAbility()){
-				computerNicScore.innerHTML = "The evil Nicolas Cage dodged your attack";
+	(function computerNicStatus(){
+		if (thePlayers[1][0].dodgeAbility() >= thePlayers[0][0].attackAbility()){
+				$theScores.append("<div>The evil Nicolas Cage dodged your attack</div>");
 				return currentComputerNicScore;
 				
 		}else{
-			currentComputerNicScore -= thePlayers[0].attackAbility();
-			computerNicScore.innerHTML = `${thePlayers[1].name}'s remaining hit points ${currentComputerNicScore}`;
+			currentComputerNicScore -= thePlayers[0][0].attackAbility();
+			$theScores.append(`<div>${thePlayers[1][0].name}'s remaining hit points ${currentComputerNicScore}</div>`);
 		}
-	}
-	computerNicStatus();
-	
+	})();	
 
 	
 
 	if (currentUserScore <= 0 ) {
-		gameResults.innerHTML = `Better luck next time! This CAGE MATCH goes to the enemy, ${thePlayers[1].name}`;
+		gameResults.innerHTML = `Better luck next time! This CAGE MATCH goes to the enemy, ${thePlayers[1][0].name}`;
+		$gameResultsContainer.css("display", "block");
 
 	}else if(currentComputerNicScore <= 0){
-		gameResults.innerHTML = `You and ${thePlayers[0].name} win this CAGE MATCH!`;
+		gameResults.innerHTML = `You and ${thePlayers[0][0].name} win this CAGE MATCH!`;
+		$gameResultsContainer.css("display", "block");
 	}
 };
 
 
 resetButton.onclick = function reset(){
 		thePlayers = [];
-		userScore.innerHTML = "";
-		computerNicScore.innerHTML = "";
+		$theScores.html("");
 		playerInfo.innerHTML = "";
 		opponentInfo.innerHTML = "";
 		roundNumber.innerHTML = "";
@@ -181,10 +216,10 @@ resetButton.onclick = function reset(){
 		currentComputerNicScore = 100;
 		gameResults.innerHTML = "";
 		round = 0;
+		$gameResultsContainer.css("display", "none");
+		var imgagesToReset = document.querySelectorAll('img');
 
-		var userSelectedPlayer = document.querySelectorAll('img');
-
-		[].forEach.call(userSelectedPlayer, function(anImg) {
+		[].forEach.call(imgagesToReset, function(anImg) {
 		    anImg.className = "";
 		});
 	}
